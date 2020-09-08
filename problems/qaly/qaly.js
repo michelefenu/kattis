ce2b21affb4c755a505d9673f9106361e6dd8d7d
Michele Fenu <michelefenu@gmail.com>
2020-09-08 15:57:35 +0000

    Add qaly solution

--- problems/qaly/qaly.js
+++ problems/qaly/qaly.js
@@ -1,16 +1,25 @@
-var readline = require('readline');
+var readline = require('readline')
 var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-});
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
 
-const input = [];
+let input = []
 
 rl.on('line', function (line) {
-    input.push(line);
-});
+  input.push(line)
+})
 
 rl.on('close', () => {
-    console.log(input)
-});
+  input = input.splice(1)
+
+  const totalQaly = input.reduce((totalQaly, line) => {
+    const q = +line.split(' ')[0]
+    const y = +line.split(' ')[1]
+
+    return totalQaly + q * y
+  }, 0)
+
+  console.log(totalQaly)
+})
